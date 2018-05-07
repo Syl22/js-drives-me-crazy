cff448991b0d9a53614ea270560323b30b78c62c
Will Djbs <will.djbs@gmail.com>
2018-05-07 11:17:54 +0200

    Ajout de fonctionnalités frontent, affichage des dossier à partir du mock, et detail sur les dossier

--- Client-App/src/app/app.module.ts
+++ Client-App/src/app/app.module.ts
@@ -3,19 +3,26 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { NgModule } from '@angular/core';
 
-import {MatButtonModule, MatCheckboxModule} from '@angular/material';
+import {MatButtonModule, MatCheckboxModule, MatDialogModule} from '@angular/material';
 import {MatInputModule} from '@angular/material/input';
 import {MatGridListModule} from '@angular/material/grid-list';
 
 import { AppComponent } from './app.component';
 import { AuthComponent } from './auth/auth.component';
 import {FormsModule} from '@angular/forms';
+import { ExplorerComponent } from './explorer/explorer.component';
+
+import { Filetype } from './filetest';
+import { FILELIST } from './mock';
+import { FiletypeDetailComponent } from './filetype-detail/filetype-detail.component'
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    AuthComponent
+    AuthComponent,
+    ExplorerComponent,
+    FiletypeDetailComponent
   ],
   imports: [
     BrowserAnimationsModule,
@@ -24,7 +31,8 @@ import {FormsModule} from '@angular/forms';
     MatButtonModule,
     MatCheckboxModule,
     MatInputModule,
-    MatGridListModule
+    MatGridListModule,
+    MatDialogModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- Client-App/src/app/auth/auth.component.ts
+++ Client-App/src/app/auth/auth.component.ts
@@ -1,4 +1,5 @@
 import {Component, OnInit} from '@angular/core';
+import {MatDialog} from '@angular/material';
 
 @Component({
   selector: 'app-auth',
@@ -8,9 +9,16 @@ import {Component, OnInit} from '@angular/core';
 
 export class AuthComponent implements OnInit {
   connected = false;
-  constructor(  ) { }
+  checkedG = false;
+  checkedO = false;
+  valInit = '';
+
+  constructor() {
+  }
 
   ngOnInit() {
   }
-
+  public reinit() {
+    this.valInit = '';
+  }
 }
